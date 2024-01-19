f2f12ee23deb1c3d8fa88fc5666f85f03a471f48
samuelpd22 <samuel050101@hotmail.com>
2024-01-19 15:11:35 -0300

    Correção LogistaDTO

--- src/main/java/com/picpay/dto/LogistaDTO.java
+++ src/main/java/com/picpay/dto/LogistaDTO.java
@@ -6,5 +6,5 @@ import jakarta.validation.Valid;
 import java.math.BigDecimal;
 
 public record LogistaDTO(@Valid String primeiroNome, String segundoNome, String cpfOuCnpj,
-                         String email, String senha, BigDecimal saldoDisponivel, TipoUsuario tipoUsuario) {
+                         String email, String senha, BigDecimal saldoDisponivel, TipoUsuario tipo) {
 }
